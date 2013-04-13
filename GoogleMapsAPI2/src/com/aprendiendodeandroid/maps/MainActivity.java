6441ce288e25cd86581a535a5b0af5401a6f635d
Jackgris <jackgris2@gmail.com>
2013-04-13 11:34:36 -0300

    Se agrego un marcador para que muestre tu ubicacion actual, y una camara para que cambie la vista del mapa

--- GoogleMapsAPI2/src/com/aprendiendodeandroid/maps/MainActivity.java
+++ GoogleMapsAPI2/src/com/aprendiendodeandroid/maps/MainActivity.java
@@ -1,5 +1,11 @@
 package com.aprendiendodeandroid.maps;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
@@ -13,10 +19,25 @@ import android.view.Menu;
  */
 public class MainActivity extends FragmentActivity {
 
+	public static GoogleMap mapa;
+	private LocationManager locationManager;
+	private MiLocationListener locationListener = new MiLocationListener();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        
+		// obtenemos el mapa desde el fragment
+		mapa = (((SupportMapFragment)getSupportFragmentManager().
+		        findFragmentById(R.id.map)).getMap());
+		
+		// seteamos para que este el boton de buscar nuestra ubicacion activado
+		mapa.setMyLocationEnabled(true);
+				
+		iniciamosLaEscucha();
 	}
 
 	@Override
@@ -25,5 +46,46 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	/** 
+	 hay que asegurarse de usar esta funcion en el hilo principal, y no en un hilo en background 
+	 hay que asegurarse de remover las actualizaciones cuando no se use mas
+	*/
+	public void iniciamosLaEscucha(){
+	  // baja precision
+	  LocationProvider low=
+	    locationManager.getProvider(locationManager.getBestProvider(MiLocationListener.
+	            crearCriteriaRed(),false));
+	 
+		  // usamos el proveedor de baja precision... y le ponemos el listener para que lo actualice
+		  locationManager.requestLocationUpdates(low.getName(), 0, 0f, locationListener);  
+	}
+
+	/**
+	 * Vamos a detener la escucha de nuestro listener y todo tipo de actualizacion	
+	 */
+	@Override
+	protected void onStop() {
+		super.onStop();
+		locationManager.removeUpdates(locationListener);
+	}
+	
+	/**
+	 * Vamos a detener la escucha de nuestro listener y todo tipo de actualizacion 
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		locationManager.removeUpdates(locationListener);
+	}
+	
+	/**
+	 * Volvemos a arrancar las actualizaciones
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		iniciamosLaEscucha();
+	}  
 
 }
